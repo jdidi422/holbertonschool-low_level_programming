51f688a532e859811233f499365bdbee81006d63
jdidi422 <rassoljdidi3@gmail.com>
2024-10-24 21:26:47 +0200

    change

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 
-int print_last_digit(int n) {
-    int last_digit = n % 10;  // Get the last digit
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to evaluate
+ *
+ * Return: The value of the last digit
+ */
+int print_last_digit(int n);  /* Function declaration */
 
-    // Handle negative numbers
-    if (last_digit < 0) {
-        last_digit = -last_digit;  // Make it positive
-    }
+int print_last_digit(int n)
+{
+	int last_digit;
 
-    printf("%d\n", last_digit);  // Print the last digit
-    return last_digit;  // Return the last digit
+	last_digit = n % 10;  /* Get the last digit */
+
+	/* Handle negative numbers */
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;  /* Make it positive */
+	}
+
+	printf("%d\n", last_digit);  /* Print the last digit */
+	return (last_digit);  /* Return the last digit */
 }
 
-int main() {
-    int number = -1234;
-    int last_digit = print_last_digit(number);
-    printf("Last digit is: %d\n", last_digit);
-    return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+	int last_digit;
+
+	number = -1234;  /* Example number */
+	last_digit = print_last_digit(number);
+	printf("Last digit is: %d\n", last_digit);
+	return (0);
 }
 
