2518c6ef3a818ad8e894cdda25b0d8f03c1d5d8d
jdidi422 <rassoljdidi3@gmail.com>
2024-10-24 15:21:23 +0200

    change

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,7 @@
-#include "main.h"
+ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+int _putchar(char c);
+void print_alphabet(void);
 
+#endif
