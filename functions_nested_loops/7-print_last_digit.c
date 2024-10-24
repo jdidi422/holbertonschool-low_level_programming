8c0137c73a98de238b15cd23467d2d9013514bd4
jdidi422 <rassoljdidi3@gmail.com>
2024-10-24 21:40:50 +0200

    change

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,42 +1,19 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * print_last_digit - Prints the last digit of a number
- * @n: The number to evaluate
- *
- * Return: The value of the last digit
+ * print_last_digit - last_digit
+ * @r: An integre input
+ * Return: 0
  */
-int print_last_digit(int n);  /* Function declaration */
 
-int print_last_digit(int n)
+int print_last_digit(int r)
 {
-	int last_digit;
+	int l;
 
-	last_digit = n % 10;  /* Get the last digit */
-
-	/* Handle negative numbers */
-	if (last_digit < 0)
+	l = r % 10;
+	if (l < 0)
 	{
-		last_digit = -last_digit;  /* Make it positive */
+		l *= -1;
 	}
-
-	printf("%d\n", last_digit);  /* Print the last digit */
-	return (last_digit);  /* Return the last digit */
+	_putchar('0' + l);
+	return (l);
 }
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int number;
-	int last_digit;
-
-	number = -1234;  /* Example number */
-	last_digit = print_last_digit(number);
-	printf("Last digit is: %d\n", last_digit);
-	return (0);
-}
-
