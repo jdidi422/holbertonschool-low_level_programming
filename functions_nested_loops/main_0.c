6802ae5e96c6eef99635afa62da8588844a69db1
jdidi422 <rassoljdidi3@gmail.com>
2024-10-26 01:55:37 +0200

    change

---
+++ functions_nested_loops/main_0.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* Function declaration */
+int print_last_digit(int n);
+int main(void)
+{
+int r;
+r = print_last_digit(98); /* You can use a traditional comment here */
+return (0);
+}
