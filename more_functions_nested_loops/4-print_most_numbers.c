072be4389d07419161e1968e0a113f8e15dbba06
jdidi422 <rassiljdidi3@gmail.com>
2024-10-29 10:59:50 +0100

    changebetty

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_numbers - Prints numbers from 0 to 9 followed by a new line - print numbers Do not print 2 and 4
+ */
+void print_numbers(void)
+{
+	char print_numbers;
+
+	for (print_numbers = '0'; print_numbers <= '9'; print_numbers++)
+		{
+        if (i != 2 && i != 4) {
+		 }
+
+	{
+		_putchar(print_numbers);
+	}
+	_putchar('\n');
+}
+
