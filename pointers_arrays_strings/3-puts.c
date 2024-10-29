0b49449ae32a911fd4126689792e82a38e3a147f
jdidi422 <rassiljdidi3@gmail.com>
2024-10-29 18:16:17 +0100

    chnage

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,24 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+
 /**
- * _strcmp - check the code
- * @s1 : char
- * @s2 : char
- * Return: dest.
+ * _puts - prints a string followed by a new line
+ * @str: input string
  */
-
-int _strcmp(char *s1, char *s2)
+void _puts(char *str)
 {
-	int i = 0;
-
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (*str != '\0')
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
+		_putchar(*str);
+		str++;
 	}
-	return (s1[i] - s2[i]);
+	_putchar('\n');
 }
