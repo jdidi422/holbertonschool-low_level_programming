fd84030730927763f15a0f0a4dc929bc53a59bbc
jdidi422 <rassiljdidi3@gmail.com>
2024-10-29 12:07:08 +0100

    chnage

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - Check description
+ * Description: print_diagonal
+ * @n : integer
+ * Return: void (Success) .
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; i > j; j++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
