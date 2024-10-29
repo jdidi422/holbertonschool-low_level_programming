c4cc7a5a09bc35f9c1fa57c4d170416c1269ab40
jdidi422 <rassiljdidi3@gmail.com>
2024-10-29 18:32:30 +0100

    chnage

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_array(int *a, int n);
 int _atoi(char *s);
 char *_strcpy(char *dest, char *src);
-void print_array(int *a, int n);
