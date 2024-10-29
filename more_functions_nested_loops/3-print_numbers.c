94ca492e61ae625fda9d255ec3ef90caadc68e0b
jdidi422 <rassiljdidi3@gmail.com>
2024-10-29 09:38:37 +0100

    change

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,10 @@ void print_numbers(void)
 {
 	char number;
 
-	for (number = '0'; number<= '9'; number++)
+	for (number = '0'; number <= '9'; number++)
 
 	{
 		_putchar(number);
 	}
 	_putchar('\n');
+}
