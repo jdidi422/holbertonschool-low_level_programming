3605afb830d420ee5d05221d2d94d22fdaebbf49
jdidi422 <rassiljdidi3@gmail.com>
2024-10-29 18:46:22 +0100

    Your commit message here

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
-#include "main.h"
-/**
- *_isupper - function that verifies if a  character is uppercase or not
+#include "main.h"*
+
+ /*_isupper - function that verifies if a  character is uppercase or not
  *@c: tested character
  *Return: returns 1 if it is uppercase , 0 if not
  */
@@ -8,6 +8,7 @@
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
+
 		return (1);
 
 	return (0);
--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,18 +5,18 @@
  */
 void more_numbers(void)
 {
-	int i, num;
+int i, num;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (num = 0; num <= 14; num++)
-		{
-			if (num > 9)
-			{
-				_putchar((num / 10) + '0');
-			}
-			_putchar((num % 10) + '0');
-		}
-		_putchar('\n');
-	}
+for (i = 0; i < 10; i++)
+{
+for (num = 0; num <= 14; num++)
+{
+if (num > 9)
+{
+_putchar((num / 10) + '0');
+}
+_putchar((num % 10) + '0');
+}
+_putchar('\n');
+}
 }
