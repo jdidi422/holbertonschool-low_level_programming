9f55b87b36d66e2129a2997ad703f9b7bb2363e4
jdidi422 <rassiljdidi3@gmail.com>
2024-10-31 11:18:21 +0100

    change

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ **_strncat-*_strncat
+ *@src:src
+ *@dest:dest
+ *@n: int
+ *Return:0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	if (n > 0)
+	{
+		while (dest[i] != '\0')
+		{
+			i++;
+		}
+		while (src[j] != '\0' && j < n)
+		{
+			dest[i] = src[j];
+			j++;
+			i++;
+		}
+	}
+	return (dest);
+}
