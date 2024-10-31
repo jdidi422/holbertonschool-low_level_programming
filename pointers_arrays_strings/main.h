0081d32f4e83812ccdc6f5bcb39e35601b55dab5
jdidi422 <rassiljdidi3@gmail.com>
2024-10-31 11:32:14 +0100

    change

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,4 +12,4 @@ char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-
+void reverse_array(int *a, int n);
