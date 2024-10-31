0185ff5ebaeff7ff015138dda1378f34006c4c4e
jdidi422 <rassiljdidi3@gmail.com>
2024-10-31 11:21:08 +0100

    change

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strncpy - check the code
+ * Description: copy @n number of array to another :)
+ * @dest : pointer to destination array
+ * @src : pointer to be copied
+ * @n : int
+ * Return: char*
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+		*(dest + i) = *(src + i);
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
