dff7cc59923efb71306f702aa3751a9885dee6b4
jdidi422 <rassiljdidi3@gmail.com>
2024-10-31 11:43:57 +0100

    chnage

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * reverse_array - check the code
+ * @a : array of integer
+ * @n : number of elements
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int str[1000];
+
+	i = 0;
+	if (n > 0)
+	{
+		do {
+			str[i] = *(a + n - i - 1);
+			i++;
+		} while (i <= n);
+		i = 0;
+		do {
+			*(a + i) = str[i];
+			i++;
+		} while (i < n);
+	}
+}
