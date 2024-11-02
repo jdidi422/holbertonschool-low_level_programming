6f778a2c0e205b0f5a17de9941a912b7826a7f25
jdidi422 <rassiljdidi3@gmail.com>
2024-11-02 09:29:11 +0100

    change

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - check the code
+ * @s: pointer to the string to be executed
+ * Return: void
+ */
+char *leet(char *s)
+{
+	int j, i, l = 0;
+	char T[5] = {'A', 'E', 'O', 'T', 'L'};
+	char V[5] = {'4', '3', '0', '7', '1'};
+
+	l = strlen(s);
+	for (i = 0; i < l; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == T[j] || s[i] == T[j] + 32)
+				s[i] = V[j];
+		}
+	}
+	return (s);
+}
