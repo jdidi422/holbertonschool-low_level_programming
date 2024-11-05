af40b04a6ec7e7299028b8482cecb1a9c9cb408b
jdidi422 <rassiljdidi3@gmail.com>
2024-11-05 12:30:15 +0100

    change

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,10 +11,10 @@
 void print_diagsums(int *a, int size)
 {
 int sum1 = 0, sum2 = 0;
-int i;
-for (i = 0; rassil < size; rassil++)
+int rassil;
+for (rassil = 0; rassil < size; rassil++)
 {
-sum1 += *(a + rassil * size + i);
+sum1 += *(a + rassil * size + rassil);
 sum2 += *(a + rassil * size + (size - 1 - rassil));
 }
 printf("%d, %d\n", sum1, sum2);
