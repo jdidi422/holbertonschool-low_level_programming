329244357ebb85fa3e0c27278ee087b746027e81
jdidi422 <rassiljdidi3@gmail.com>
2024-11-12 11:01:46 +0100

    chnage

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,37 +1,23 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * _strdup - Duplicates a string by allocating memory and copying the content
- *           of the original string into it.
- * @str: The string to duplicate
- *
- * Return: A pointer to the duplicated string or NULL if allocation fails or
- *         if str is NULL.
+ * _strdup - Entry point
+ * @str : char
+ * Return: void (Success)
  */
 char *_strdup(char *str)
 {
-int jdidi = 0, size = 0;
-char *rassil;
+int i = 0;
 if (str == NULL)
 {
 return (NULL);
 }
-while (str[size] != '\0')
+while (str[i] != '\0')
 {
-size++;
+i++;
 }
-char *rassil = malloc(size *sizeof(*tr) + 1);
-if (rassil == NULL)
-{
-return (NULL);
-}
-while (jdidi < size)
-{
-rassil[jdidi] = str[jdidi];
-jdidi++;
-}
-rassil[size] = '\0';
-return (rassil);
+printf("%s", str);
+str = malloc(sizeof(char) * i + 1);
+return (str);
 }
