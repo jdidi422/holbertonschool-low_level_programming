1dfba3ef3a08f93f7856cbdf7d2d508394ce23db
jdidi422 <rassiljdidi3@gmail.com>
2024-11-12 14:11:19 +0100

    change

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,16 +1,32 @@
-#include "main.h"
-
+#include <stdlib.h>
 /**
- * _strlen_recursion - returns the length of a string
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
  *
- * @s: the string to check the length of
- *
- * Return: an integer that displays the length of the string
+ * Return: Pointer to a newly allocated space in memory which contains
+ * the contents of s1, followed by s2, and null terminated. If NULL
+ * is passed, treat it as an empty string. Returns NULL on failure.
  */
-
-int _strlen_recursion(char *s)
+char *str_concat(char *s1, char *s2)
 {
-if (*s != '\0')
-return (1 + _strlen_recursion(s + 1));
-return (0);
+int len1 = 0, len2 = 0, i, j;
+char *concat_str;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[len1] != '\0')
+len1++;
+while (s2[len2] != '\0')
+len2++;
+concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
+if (concat_str == NULL)
+return (NULL);
+for (i = 0; i < len1; i++)
+concat_str[i] = s1[i];
+for (j = 0; j < len2; j++)
+concat_str[i + j] = s2[j];
+concat_str[i + j] = '\0';
+return (concat_str);
 }
