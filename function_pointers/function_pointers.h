a758ace31b63d172eec2242597afe4391131d6a2
jdidi422 <rassiljdidi3@gmail.com>
2024-11-20 12:08:19 +0100

    goo

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#include <stddef.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
