036e347897fff1ac9559f2d9cc2ceb94f99347b9
jdidi422 <rassiljdidi3@gmail.com>
2024-11-21 09:56:48 +0100

    chnider1

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,23 +1,24 @@
 #include "variadic_functions.h"
+
 /**
- * sum_them_all - check the code
- * @n: unsigned int
- * Return: Always 0.
+ * sum_them_all - Returns the sum of all its parameters.
+ * @n: Number of arguments passed to the function.
+ *
+ * Return: Sum of parameters, or 0 if n == 0.
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-va_list list;
-unsigned int i, sum = 0;
+va_list args;
+unsigned int i;
+int sum = 0;
 if (n == 0)
-{
+
 return (0);
-}
-va_start(list, n);
+va_start(args, n);
 for (i = 0; i < n; i++)
-{
-sum += va_arg(list, int);
-}
-va_end(list);
+sum += va_arg(args, int);
+va_end(args);
 return (sum);
 }
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,20 +1,31 @@
-#ifndef __VARIADIC_FUNCTIONS__
-#define __VARIADIC_FUNCTIONS__
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdarg.h>
-
-typedef struct types
-{
-    char type;
-    void (*function)( va_list);
-} types_t;
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char *const format, ...);
+
+/**
+ * struct print_func - a key for picking functions to use for printing
+ * in 3-print_all.c
+ *
+ * @print: function to use for printing
+ * @spec: character specifier
+ */
+typedef struct print_func
+{
+void (*print)(va_list args);
+char spec;
+} print_func;
+
+
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+void print_all(const char * const format, ...);
 
 #endif
