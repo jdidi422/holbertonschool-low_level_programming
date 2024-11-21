e2088efe9c4147bfe3e78b9a49d93c0e95416043
jdidi422 <rassiljdidi3@gmail.com>
2024-11-21 09:53:58 +0100

    chnider1

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -13,4 +13,8 @@ typedef struct types
 } types_t;
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char *const format, ...);
+
 #endif
