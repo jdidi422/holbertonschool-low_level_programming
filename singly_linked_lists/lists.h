7de3d2ac6cfcb3503ea809ef96a0598a6e6981e8
jdidi422 <rassiljdidi3@gmail.com>
2024-12-03 15:06:23 +0100

    putchar

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,10 +1,8 @@
 #ifndef LISTS_H
-#define Lists_H
+#define LISTS_H
 
-#include <stidio.h>
-#include<stdlib.h>
-#include <string.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -16,10 +14,18 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
+
 int _putchar(char c);
-int _strelen(char *s);
+int _strlen(char *s);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+
+#endif
