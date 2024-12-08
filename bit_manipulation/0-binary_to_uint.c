9ddc0939f137d2c52435bf4a1e8e2e7fc6e36db1
jdidi422 <rassiljdidi3@gmail.com>
2024-12-08 10:04:55 +0100

    bit

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -2,22 +2,26 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Convertit une chaîne binaire en entier non signé.
- * @b: Chaîne de caractères contenant des '0' et '1'.
- * Return: La valeur entière convertie, sinon 0 en cas d'erreur.
+ * binary_to_uint - Function that converts a binary number to an unsigned int.
+ * Prototype: unsigned int binary_to_uint(const char *b);
+ * @b: is pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if
+ * -> there is one or more chars in the string b that is not 0 or 1
+ * -> b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int decimal = 0;
-int i = 0;
-if (!b)
+unsigned int value = 0;
+if (b == NULL)
 return (0);
-while (b[i] != '\0')
+while (*b != '\0')
 {
-if (b[i] != '0' && b[i] != '1')
+value = value << 1;
+if (*b != '1' && *b != '0')
 return (0);
-decimal = decimal * 2 + (b[i] - '0');
-i++;
+else if (*b == '1')
+value = value | 1;
+b++;
 }
-return (decimal);
+return (value);
 }
