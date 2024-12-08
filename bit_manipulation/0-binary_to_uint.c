7304654031a4d23907eb4fc22e1b86a4790bda99
jdidi422 <rassiljdidi3@gmail.com>
2024-12-08 19:14:10 +0100

    change

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -2,15 +2,16 @@
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: Pointer to a string of 0 and 1 chars.
+ * @b: Pointer to a string of 0 and 1 chars representing a binary number.
  *
- * Return: The converted number, or 0 if invalid.
+ * Return: The converted unsigned int value, or 0 if input is invalid.
  */
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int num = 0;
 int i;
 if (b == NULL)
+
 return (0);
 for (i = 0; b[i] != '\0'; i++)
 {
