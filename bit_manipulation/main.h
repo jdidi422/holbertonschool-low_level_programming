98c821c3ea45ce78b5c9f41d3acfeee1fc5709e1
jdidi422 <rassiljdidi3@gmail.com>
2024-12-08 19:04:26 +0100

    change

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,10 +1,14 @@
-#ifndef MAIN
-#define MAIN
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdlib.h>
+#include <stddef.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
