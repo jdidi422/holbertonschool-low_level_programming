1e64210f1c80c37754aa643c918ab37156516bb9
jdidi422 <rassiljdidi3@gmail.com>
2024-12-08 18:55:52 +0100

    chnage

--- file_io/0-main.c
+++ file_io/0-main.c
@@ -5,8 +5,12 @@
 /**
  * main - check the code
  *
+ * @ac: The number of command-line arguments.
+ * @av: Array of command-line argument strings.
+ *
  * Return: Always 0.
  */
+
 int main(int ac, char **av)
 {
 ssize_t n;
